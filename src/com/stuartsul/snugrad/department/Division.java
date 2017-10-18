aceff94defd6a4e949be599743aad6eacd4170c2
StuartSul <ssstuartss@gmail.com>
2017-10-18 15:26:05 +0900

    added division information

---
+++ src/com/stuartsul/snugrad/department/Division.java
@@ -0,0 +1,28 @@
+package com.stuartsul.snugrad.department;
+
+/* All information regarding divisions and departments of
+   Seoul National University was retrieved from
+   http://www.snu.ac.kr/colleges on October 18th, 2017 */
+
+public class Division {
+
+  /* Divisons */
+  public static final int unknown = -1;
+  public static final int humanities = 0;         // 인문대학
+  public static final int social_science = 1;     // 사회과학대학
+  public static final int natural_science = 2;    // 자연과학대학
+  public static final int nursing = 3;            // 간호대학
+  public static final int business = 4;           // 경영대학
+  public static final int engineering = 5;        // 공과대학
+  public static final int agriculture = 6;        // 농업생명과학대학
+  public static final int art = 7;                // 미술대학
+  public static final int law = 8;                // 법과대학
+  public static final int education = 9;          // 사범대학
+  public static final int human_ecology = 10;     // 생활과학대학
+  public static final int vet = 11;               // 수의과대학
+  public static final int pharmacy = 12;          // 약학대학
+  public static final int music = 13;             // 음악대학
+  public static final int medicine = 14;          // 의과대학
+  public static final int liberal = 15;           // 자유전공학부
+  public static final int interdisciplinary = 16; // 연계, 연합전공
+}
