e874458827de2fbba0dc79208b2c3d0885ed24f6
StuartSul <ssstuartss@gmail.com>
2017-10-18 18:12:19 +0900

    added department class

---
+++ src/com/stuartsul/snugrad/department/Department.java
@@ -0,0 +1,11 @@
+package com.stuartsul.snugrad.department;
+
+/* All information regarding divisions and departments of
+   Seoul National University was retrieved from
+   http://www.snu.ac.kr/colleges on October 18th, 2017 */
+
+public class Department {
+
+  /* Departments */
+  public static final int unknown = -1;
+}
