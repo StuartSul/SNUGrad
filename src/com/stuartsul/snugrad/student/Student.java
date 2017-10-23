f87351465a295f08598fe1ae8a6836df8386cc66
StuartSul <ssstuartss@gmail.com>
2017-10-23 20:25:43 +0900

    Created new fields for Student class

--- src/com/stuartsul/snugrad/student/Student.java
+++ src/com/stuartsul/snugrad/student/Student.java
@@ -1,20 +1,35 @@
 package com.stuartsul.snugrad.student;
 
+import com.stuartsul.snugrad.department.Department;
+
 public class Student {
 
   /* General Student Information */
-  String firstName;
-  String lastName;
-  int studentID;
-  int classYear;           // "학번"
-  boolean isFallClass;     // ".5학번"
-  int department;          // "입학전공"
+  private String first_name;
+  private String last_name;
+  private int student_ID;
+  private int class_year;         // 학번
+  private boolean isFallClass;    // .5학번
+  private Department department;  // 입학전공
 
-  /* Concentraion */
-  int[] major_single;      // "단일/심화전공"
-  int[] major_main;        // "주전공"
-  int[] major_double;      // "복수전공"
-  int[] minor;             // "부전공"
-  int[] inter; // "연계전공"
+  /* Concentraion 전공 */
+  private Department[] major_single;  // 단일/심화전공
+  private Department[] major_main;    // 주전공
+  private Department[] major_double;  // 복수전공
+  private Department[] minor;         // 부전공
+  private Department[][] concentration;
 
+  /* Course Registration History 이수 기록 */
+  private Semester[] semesters;
+
+  /* Constructor */
+  public Student() {
+
+  }
+
+  /* Set-Functions */
+  /* Add-Functions */
+  /* Get-Functions */
+  /* Delete-Functions */
+  /* File IO */
 }
